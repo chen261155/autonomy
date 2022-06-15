02dc7b406e33cf7e246f8394ccc7c4c4ad1802ca
chen261155 <1554758745@qq.com>
2022-06-15 14:53:28 +0800

    add interview project txt

--- interview/src/main/java/com/test/interview/memberandlocalvariables/Exam5.java
+++ interview/src/main/java/com/test/interview/memberandlocalvariables/Exam5.java
@@ -1,24 +1,24 @@
 package com.test.interview.memberandlocalvariables;
 
 public class Exam5 {
-    static int s;
-    int i;
-    int j;
+    static int s;//成员变量，类变量
+    int i;//成员变量， 实例变量
+    int j;//成员变量， 实例变量
     {
-        int i = 1;
+        int i = 1;//费静态代码块中的局部变量i
         i++;
         j++;
         s++;
     }
-    public void test(int j){
+    public void test(int j){//形参，局部变量，j
         i++;
         j++;
         s++;
     }
 
     public static void main(String[] args) {
-        Exam5 obj1 = new Exam5();
-        Exam5 obj2 = new Exam5();
+        Exam5 obj1 = new Exam5();//局部变量obj1
+        Exam5 obj2 = new Exam5();//局部变量obj2
         obj1.test(10);
         obj1.test(20);
         obj2.test(30);
--- interview/src/main/java/com/test/interview/recursionanditeration/Iteration.java
+++ interview/src/main/java/com/test/interview/recursionanditeration/Iteration.java
@@ -26,5 +26,6 @@ public class Iteration {
             one = sum;
         }
         return sum;
+
     }
 }
